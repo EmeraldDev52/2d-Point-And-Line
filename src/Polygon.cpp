b2248b1918165d67c899b2d66d3b0f5935dc2c7e
EmeraldDev52 <brooks.github@gmail.com>
2025-06-09 18:01:35 -0500

    Update Polygon.cpp

--- src/Polygon.cpp
+++ src/Polygon.cpp
@@ -21,10 +21,15 @@ namespace Geo2d{
         for (int i = 0; i < sides; ++i)
         {
             double angle = (i * angleStep) + (M_PI / sides) - rotationOffset * DEG2RAD;
-            double x = origin.x + scale.x * circumRadius * std::cos(angle);
-            double y = origin.y + scale.y * circumRadius * std::sin(angle);
-            m_vertices.push_back(Vector2d(x, y));
+        
+            double x = circumRadius * std::cos(angle);
+            double y = circumRadius * std::sin(angle);
+
+            // apply scale then translate to origin
+            Vector2d scaled = Vector2d(x * scale.x, y * scale.y);
+            m_vertices.push_back(origin + scaled);
         }
+
 
     }
 
