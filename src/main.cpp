495152891f258a4afae49a0ee728a260984550ee
EmeraldWeb52 <brooks.github@gmail.com>
2025-06-11 14:00:58 -0500

    removed main.cpp

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/Geo2d/Vector2d.h"
-#include "../include/Geo2d/BoundingBox.h"
-#include "../include/Geo2d/Line2d.h"
-#include "../include/Geo2d/Polygon.h"
-
-int main() {
-
-	return 0;
-}
